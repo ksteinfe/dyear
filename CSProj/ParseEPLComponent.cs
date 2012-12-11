a724c72e6e6e682a79821c5499e2c0e49cebaad1
Kyle Steinfeld <ksteinfe@berkeley.edu>
2012-12-11 01:10:14 -0800

    got basic GUI worked out

--- CSProj/ParseEPLComponent.cs
+++ CSProj/ParseEPLComponent.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Forms;
+using System.Diagnostics;
 using Grasshopper.Kernel;
 using Rhino.Geometry;
 using System.Drawing;
@@ -15,6 +16,7 @@ namespace DYear
     {
         //http://www.grasshopper3d.com/forum/topics/can-the-solveinstance-method?commentId=2985220%3AComment%3A107325
         private int header_lines = 1;
+        private string filepath = "";
 
         /// <summary>
         /// a dictionary of dictionaries of ints that describe the column structure of the loaded EPL data file
@@ -40,42 +42,37 @@ namespace DYear
 
         protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
         {
-            pManager.Register_StringParam("EP Output File", "EPW", "The path to the CSV file to be parsed",GH_ParamAccess.item);
+            //pManager.Register_StringParam("EP Output File", "EPW", "The path to the CSV file to be parsed",GH_ParamAccess.item);
         }
 
         protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
         {
-
-            GHParam_DHr param = new GHParam_DHr();
-            pManager.RegisterParam(param,"Hours","Dhr","The hourly EPW data served up as a list of DHr objects",GH_ParamAccess.list);
             pManager.Register_StringParam("headers", "headers", "for testing");
             
+            GHParam_DHr param = new GHParam_DHr();
+            pManager.RegisterParam(param, "Hours", "Dhr", "The hourly EPW data served up as a list of DHr objects", GH_ParamAccess.list);
         }
 
         protected override void SolveInstance(IGH_DataAccess DA)
         {
-            string filepath = "";
+            // simply passes data stored in zone_hours dict to output params
 
-            if (DA.GetData(0, ref filepath)) //if it works...
+            if (zone_hours.Count > 0)
             {
-                DictClear();
-                DictParseFilepath(filepath);
-
-
+                // create a list of all the column headers we've found, and pass these to the 'headers' output 
                 List<string> zonenames = new List<string>();
-                foreach (KeyValuePair<string, List<DHr>> entry in zone_hours)
-                {
-                    zonenames.Add(entry.Key);
-                }
+                foreach (KeyValuePair<string, List<DHr>> entry in zone_hours) {zonenames.Add(entry.Key);}
+                DA.SetDataList(0, zonenames);
 
-                DA.SetDataList(1, zonenames);
-
+                // for each entry in zone_hours, pass the list of dhrs to the proper output
+                //TODO
             }
         }
 
-        private void DictParseFilepath(string filepath)
+        private void DictParseFilepath()
         {
             zone_dict = new Dictionary<string, Dictionary<string, int>>();
+ 
             // get a list of the column headers
             List<string> headers;
             using (StreamReader headreader = new StreamReader(filepath)) { headers = new List<string>(headreader.ReadLine().Split(',')); }
@@ -93,8 +90,7 @@ namespace DYear
                 }
             }
 
-
-
+            zone_hours = new Dictionary<string, List<DHr>>();
             foreach (string key in zone_dict.Keys) { zone_hours.Add(key, new List<DHr>()); } // empty list of hours for each zone
 
             StreamReader reader = new StreamReader(filepath);
@@ -131,32 +127,58 @@ namespace DYear
 
         private void DictClear()
         {
+            zone_dict = new Dictionary<string, Dictionary<string, int>>();
             zone_hours = new Dictionary<string, List<DHr>>();
         }
 
+        private bool FilepathValid() { return File.Exists(this.filepath); }
 
-        public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
+        public override bool AppendMenuItems(ToolStripDropDown menu)
         {
-            base.AppendAdditionalMenuItems(menu);
+            //base.AppendMenuItems(menu);
+            Menu_AppendEnableItem(menu);
+            Menu_AppendWarningsAndErrors(menu);
+            Menu_AppendObjectHelp(menu);
             Menu_AppendSeparator(menu);
+
+            if (FilepathValid()) { Menu_AppendItem(menu, this.filepath, Menu_FilepathClicked); }
+            if (FilepathValid()) { Menu_AppendItem(menu, "Refresh Filepath", Menu_RefreshFilepathClicked); }
             Menu_AppendItem(menu, "Set Filepath", Menu_SetFilepathClicked);
+
+            return true;
         }
 
         private void Menu_SetFilepathClicked(Object sender, EventArgs e)
         {
+            OpenFileDialog fpDialog = new OpenFileDialog();
+            //fpDialog.InitialDirectory = "c:\\";
+            fpDialog.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+            fpDialog.FilterIndex = 1;
+            fpDialog.RestoreDirectory = true;
+            fpDialog.CheckFileExists = true;
+            fpDialog.CheckPathExists = true;
+
+            if ((fpDialog.ShowDialog() == DialogResult.OK) && (File.Exists(fpDialog.FileName)))
+            {
+                filepath = fpDialog.FileName;
+                this.NickName = Path.GetFileNameWithoutExtension(filepath);
+                Menu_RefreshFilepathClicked(sender, e);
+            }
+
+        }
+        private void Menu_RefreshFilepathClicked(Object sender, EventArgs e)
+        {
+            DictClear();
+            DictParseFilepath();
+
             GHParam_DHr param = new GHParam_DHr();
             this.Params.RegisterOutputParam(param);
             this.Params.OnParametersChanged();
             this.OnAttributesChanged();
 
-            // none of these worked to redraw this component
-            //this.ClearData();
-            //this.ExpirePreview(true);
-            //this.Attributes.ExpireLayout();
-
-            Rhino.RhinoApp.WriteLine("CLICKED!");
+            ExpireSolution(true);
         }
-        
+        private void Menu_FilepathClicked(Object sender, EventArgs e) { System.Diagnostics.Process.Start("explorer.exe", @"/select, " + this.filepath); }
         
         public override Guid ComponentGuid { get { return new Guid("{ECA812CB-6007-41B5-90EA-A3E19F5CD9AF}"); } }
 
